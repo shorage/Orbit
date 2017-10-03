c9f7992023580f3423d2ddbe64b22803fad5e335
shorage <frankanthonycosta@gmail.com>
2017-10-03 17:58:33 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "node01.orbitcrypt.com:8080",
-  "node02.orbitcrypt.com:8080",
+  "node01.orbitcrypto.com:8080",
+  "node02.orbitcrypto.com:8080",
 };
 
 struct CheckpointData {
